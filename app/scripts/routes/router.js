4972053cc5aeafa3365da9345a697d8bf99d30e2
Jason Divock <jdivock@gmail.com>
2013-07-03 18:04:48 -0400

    fixed title bar issue

--- app/scripts/routes/router.js
+++ app/scripts/routes/router.js
@@ -2,34 +2,3 @@ App.Router.map(function() {
 	this.route("blag");
 	this.route("multipost");
 });
-
-
-/*
-App.IndexMultiPostRoute = Em.Route.extend({
-	enter: function() {
-		console.log('multiPost route');
-		$(document).attr("title", "MultiPost");
-	},
-	renderTemplate: function() {
-		this.render('multipost');
-	}
-});
-
-App.IndexBlagRoute = Em.Route.extend({
-	enter: function() {
-		console.log('blag route');
-		$(document).attr("title", "Bio");
-	},
-	renderTemplate: function() {
-		this.render('bio');
-	}
-});
-
-App.IndexRoute = Em.Route.extend({
-	enter: function() {
-		console.log('index route');
-	},
-	redirect: function() {
-		this.transitionTo('index.home');
-	}
-});*/
